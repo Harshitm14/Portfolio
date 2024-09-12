cefbdfadfebd6025422e958ef8dda1ec602700b2
Harshitm14 <harshitmania3@gmail.com>
2024-09-12 07:20:32 +0530

    Refactor contact form inputs and add form attributes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -49,16 +49,16 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/*form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" netlify>
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
               I'd love to connect! Whether for collaboration, questions, or opportunities, feel free to drop a message below or reach out via email. I'll get back to you as soon as possible.              </p>
               {/*input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email Address" />
-                <Input type="phone" placeholder="Phone number" />
+                <Input type="text" name="fname" placeholder="Firstname" />
+                <Input type="text" name="lname" placeholder="Lastname" />
+                <Input type="email"  name="email" placeholder="Email Address" />
+                <Input type="number" name="phone" placeholder="Phone number" />
               </div>
               {/*select */}
               <Select>
@@ -79,9 +79,11 @@ const Contact = () => {
               <Textarea 
                 className="h-[200px]"
                 placeholder="Type your message here."
+                name="message"
+                type="text"
               />
               {/*Btn */}
-              <Button size="md" className="max-w-40">
+              <Button type="submit" size="md" className="max-w-40">
                 Send Message
               </Button>
             </form>
